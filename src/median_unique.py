1ad7f19f915934a9c61023952a63121ac0e3ee61
Victor Chiapaikeo <vchiapaikeo@gmail.com>
2015-07-18 20:27:24 -0400

    fixed docs and removed batch reset

--- src/median_unique.py
+++ src/median_unique.py
@@ -1,15 +1,33 @@
+"""Module defines functions used to efficiently calculate a streaming median
+
+The streaming median is calculated by keeping two priority queues (using Python's
+heapq module) and leveraging a property associated with heaps where the root is
+always the smallest element.
+
+One queue will contain all numbers less than or equal to the median (low) while
+the other queue will contain all numbers greater than or equal to the median. 
+
+To calculate a running median at any given point, we need to know at most two values.
+In the case of an odd number of elements, the element at the 50th percentile is
+the median whereas in the case of an even number of elements, the average of the 
+two elements surrounding the 50th percentile is the effective median.
+
+See heapq documentation for more details
+https://docs.python.org/2/library/heapq.html
+
+
+"""
 import os
 import sys
 import heapq
 
 
-def get_streaming_median(filein, batch_reset_size=500):
+def get_streaming_median(filein):
     """
     Produces a streaming median from file's unique word count per line
 
     Parameters:
         filein - source file to iterate through
-        batch_reset_size - optional param to reset arrays and reduce memory footprint; if none, do not reset
 
     Yields:
         int: the next median value
@@ -28,7 +46,6 @@ def get_streaming_median(filein, batch_reset_size=500):
 
             # Step 1: Push new values to priority queue based on low queue root
             # In priority queue, the zero index (root) will always be the min value in the array
-            # See heapq algorithm docs here: https://docs.python.org/2/library/heapq.html
             # Negate any values going to the low queue so that root is the median
             if not low or (num <= low[0]*-1):
                 heapq.heappush(low, num*-1)
@@ -37,46 +54,32 @@ def get_streaming_median(filein, batch_reset_size=500):
                 heapq.heappush(high, num)
                 high_i += 1
 
-
             # Step 2: Perform re-balancing
             # If high has more elements than low, move high root over to low and increment/decrement counters
             if low_i < high_i:
                 heapq.heappush(low, heapq.heappop(high)*-1)
                 low_i += 1
                 high_i -= 1
+
             # If low has more elements than high+1, move low root over to high and increment/decrement counters
             elif low_i > high_i+1:
                 heapq.heappush(high, heapq.heappop(low)*-1)
                 low_i -= 1
                 high_i += 1
 
-
             # Step 3: Calculate median
             # When sum of counters is even, median is equal to the avg of low and high root
             # Also, turn this into a float with precision 1 decimal
             if (low_i + high_i) % 2 == 0:
                 median = (low[0]*-1 + high[0]) / float(2)
-
-                # Perform reset action
-                if batch_reset_size is not None and ((low_i + high_i) % batch_reset_size == 0):
-                    low = [low[0]]
-                    high = [high[0]]
-                    low_i, high_i = (0, 0)
-
             # When sum of counters is odd or there is only one tweet, then median is equal to low root
             # Also, turn this into a float with precision 1 decimal
             else:
                 median = low[0]*float(-1)
 
-                # Perform reset action
-                if batch_reset_size is not None and ((low_i + high_i) % batch_reset_size == 0):
-                    low = [low[0]]
-                    high = [high[0]]
-                    low_i, high_i = (1, 0)
-
             # Generate medians one at a time
             yield median
-
+            
 
 def write_streaming_values(fileout, gen):
     """
